8a5f47fa935892da73b1448689d1163308cd914a
Youhana-Hana <meyouhana@gmail.com>
2013-05-21 20:52:49 +0100

    add tests and coverage check

---
+++ build/commit.sh
@@ -0,0 +1,14 @@
+set -e
+
+cd ./src
+
+echo 'building project'
+/usr/dev/android-sdk-linux/tools/android update project -p .
+ant clean release
+
+echo 'running android lint'
+/usr/dev/android-sdk-linux/tools/lint -Werror --exitcode .
+
+echo 'running tests'
+cd ../test
+ant build-project test
---
+++ test/src/com/MobiSeeker/PrescriptionWatcher/GivenAnActivity.java
@@ -0,0 +1,32 @@
+package com.MobiSeeker.PrescriptionWatcher;
+
+import android.content.Context;
+import com.xtremelabs.robolectric.Robolectric;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import com.xtremelabs.robolectric.shadows.ShadowApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertNotNull;
+
+@RunWith(RobolectricTestRunner.class)
+public class GivenAnActivity {
+    private ShadowApplication shadowApplication;
+
+    private Context context;
+
+    @Before
+    public void setUp() throws Exception {
+        this.shadowApplication = Robolectric.getShadowApplication();
+        this.context = shadowApplication.getApplicationContext();
+    }
+
+    @Test
+    public void whenCallingOnCreateShouldNotThrow() {
+        PrescriptionWatcher activity = new PrescriptionWatcher();
+        activity.onCreate(null);
+        assertNotNull(activity);
+    }
+
+}
