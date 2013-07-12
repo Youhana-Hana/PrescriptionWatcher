0395d7e5f71a51e54a5a1201a8b494e157da274e
youssefmyh <youssefmyh@gmail.com>
2013-07-12 20:24:40 +0200

    Adding Connection Constants

---
+++ src/src/com/MobiSeeker/PrescriptionWatcher/connection/ConnectionConstant.java
@@ -0,0 +1,17 @@
+package com.MobiSeeker.PrescriptionWatcher.connection;
+
+public class ConnectionConstant {
+	 public static final String TAG = "[Chord][ApiService]";
+
+	 public static final String TAGClass = "ChordChannelListener : ";
+
+	 public static final String SEND_MESSAGE = "SEND_MESSAGE";
+	 public static final String RECIEVE_MESSAGE = "RECIEVE_MESSAGE";
+
+	 public static final String REQUEST_FOR_REGISTER_ALARAM = "REQUEST_FOR_REGISTER_ALARAM";
+	 public static final String ALARAM_REGISTERED = "ALARAM_REGISTERED";
+	 public static final String CONFIRMED_TAKEN_MIDICEN = "CONFIRMED_TAKEN_MIDICEN";
+	 public static final String REQUEST_FOR_TAKE_MEDICIEN = "REQUEST_FOR_TAKE_MEDICIEN";
+	 public static final String GET_MY_LOCATION = "GET_MY_LOCATION";
+
+}
