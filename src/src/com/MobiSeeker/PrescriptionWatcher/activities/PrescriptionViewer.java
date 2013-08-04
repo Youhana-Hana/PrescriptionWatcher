a25a587f2e1ba372324b097f4e8ebddc12719233
Youhana-Hana <meyouhana@gmail.com>
2013-08-04 15:30:39 +0100

    complete EnteryViewer

--- src/src/com/MobiSeeker/PrescriptionWatcher/activities/PrescriptionViewer.java
+++ src/src/com/MobiSeeker/PrescriptionWatcher/activities/PrescriptionViewer.java
@@ -43,10 +43,47 @@ import roboguice.inject.InjectView;
 
 public class PrescriptionViewer extends BaseActivity  {
 
-    protected final static String TAG = "com.MobiSeeker.PrescriptionWatcher.activities.Prescription";
+    protected final static String TAG = "com.MobiSeeker.PrescriptionWatcher.activities.PrescriptionViewer";
 
-    public static void start(Context context) {
-        Intent intent = new Intent(context, PrescriptionViewer.class);
+    protected
+    @InjectView(R.id.drugName)
+    TextView drugName;
+
+    protected
+    @InjectView(R.id.comment)
+    TextView comment;
+
+    protected
+    @InjectView(R.id.timesPerDay)
+    TextView timesPerDay;
+
+    protected
+    @InjectView(R.id.dosage)
+    TextView dosage;
+
+    protected
+    @InjectView(R.id.startDate)
+    TextView startDate;
+
+    protected
+    @InjectView(R.id.endDate)
+    TextView endDate;
+
+    protected
+    @InjectView(R.id.startTime)
+    TextView startTime;
+
+    protected
+    @InjectView(R.id.endTime)
+    TextView endTime;
+
+    protected
+    @InjectView(R.id.image)
+    ImageView image;
+
+    public static void start(Context context, Entry entry) {
+        Intent intent = new Intent(context, Prescription.class);
+        intent.putExtra("entry", entry);
         context.startActivity(intent);
     }
 
@@ -54,8 +91,10 @@ public class PrescriptionViewer extends BaseActivity  {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.entryviewer);
+        Intent intent = getIntent();
+        Entry entry = (Entry)intent.getSerializableExtra("entry");
 
-        init();
+        initFromEntry(entry);
         setCurrentRoboActivity(this);
     }
     
@@ -70,14 +109,28 @@ public class PrescriptionViewer extends BaseActivity  {
     	super.onResume();
     	setCurrentRoboActivity(this);
     }
-    
-    private void init() {
-       
-    }
 
     private void initFromEntry(Entry entry) {
-       
-    }
+        this.drugName.setText(entry.getMedicineName());
+        this.dosage.setText(String.valueOf(entry.getDosage()));
+        this.timesPerDay.setText(String.valueOf(entry.getTimesPerDay()));
 
-   
+        String entryStartDate = DateFormat.getDateInstance(DateFormat.MEDIUM).format(entry.getStartDate());
+        startDate.setText(entryStartDate);
+
+        String entryEndDate = DateFormat.getDateInstance(DateFormat.MEDIUM).format(entry.getEndDate());
+        startDate.setText(entryEndDate);
+
+        LocalTime localStartTime = new LocalTime(entry.getStartTime());
+        startTime.setText(localStartTime.toString("HH:mm:ss"));
+
+        LocalTime localEndTime = new LocalTime(entry.getEndTime());
+        endTime.setText(localEndTime.toString("HH:mm:ss"));
+
+        String imagePath = entry.getPrescriptionImagePath();
+
+        if (imagePath != null && !imagePath.isEmpty()) {
+            image.setImageURI(Uri.parse(imagePath));
+        }
+    }
 }
